53f5e863b777372b3b15dfc6c5362e444536533f
christinewang766 <christinewang766@gmail.com>
2024-07-29 20:35:50 -0700

    beginning of brennis hbd site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Background from "./images/polka.jpg";
+import Brennis from "./images/brennis.png";
+import HBD from "./images/hbd.gif";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: `url(${Background})`,
+        backgroundRepeat: "repeat-x-y",
+        backgroundSize: "600px",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap:'40px',
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/* top row */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src={Brennis}
+            style={{
+              height: "100px",
+            }}
+            alt="brennis"
+          />
+        </div>
+
+        {/* middle row */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "40px",
+          }}
+        >
+          <img
+            src={Brennis}
+            style={{
+              height: "400px",
+            }}
+            alt="brennis"
+          />
+          <img
+            src={HBD}
+            style={{
+              height: "400px",
+            }}
+            alt="hbd"
+          />
+        </div>
+        {/* bottom row */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img
+            src={Brennis}
+            style={{
+              height: "100px",
+            }}
+            alt="brennis"
+          />
+        </div>
+      </div>
     </div>
   );
 }
